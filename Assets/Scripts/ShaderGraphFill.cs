6cf37a3966bfc54316b31ec57c6fcea4a5a72cab
Maxich <sasha1936@list.ru>
2021-06-03 23:39:55 +0300

    Сode cleaning

--- Assets/Scripts/ShaderGraphFill.cs
+++ Assets/Scripts/ShaderGraphFill.cs
@@ -6,24 +6,28 @@ using UnityEngine.UI;
 public class ShaderGraphFill : MonoBehaviour
 {
     public Slider slider;
-    private float _FillRateValue = 0.65f;
+    private float fillRateValue;
     private float progressBorder = 2.5f;
+    private float borderPlanet = 0.65f;
+    private float maxRange = 1.3f;
     
     Material objectMaterial;
 
     void Start()
     {
-        if (slider.value < 0.65f)
-            objectMaterial.SetFloat("FillRate", 1.3f - slider.value);
+        fillRateValue = borderPlanet;
+        
+        if (slider.value < borderPlanet)
+            objectMaterial.SetFloat("FillRate", maxRange - slider.value);
         
         objectMaterial = GetComponent<Renderer>().material;
-        objectMaterial.SetFloat("FillRate", _FillRateValue);
+        objectMaterial.SetFloat("FillRate", fillRateValue);
         objectMaterial.SetFloat("ProgressBorder", progressBorder);
     }
     
     public void ChangeValue()
     {
-        if (slider.value < 0.65f)
-            objectMaterial.SetFloat("FillRate", 1.3f - slider.value);
+        if (slider.value < borderPlanet)
+            objectMaterial.SetFloat("FillRate", maxRange - slider.value);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Test-AR
+Unity version: 2020.3.0f1
